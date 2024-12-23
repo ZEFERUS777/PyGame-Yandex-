338de9c353380de3fb2161d0b83a82ea31ed264d
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-12-23 16:34:49 +0300

    complete task

--- PG3/P1.py
+++ PG3/P1.py
@@ -1,11 +1,12 @@
 import sys
-
 import pygame
 
 pygame.init()
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 
 class Board:
@@ -16,22 +17,35 @@ class Board:
         self.left = left
         self.top = top
         self.cell_size = cell_size
-        self.screen = pygame.display.set_mode((500, 500))
+        self.screen = pygame.display.set_mode(
+            (self.width * self.cell_size + 2 * self.left, self.height * self.cell_size + 2 * self.top))
 
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
+        self.screen = pygame.display.set_mode(
+            (self.width * self.cell_size + 2 * self.left, self.height * self.cell_size + 2 * self.top))
 
     def render(self, screen):
         for x in range(self.width):
             for y in range(self.height):
+                color = self.get_color(self.board[y][x])
                 rect = pygame.Rect(self.left + x * self.cell_size, self.top + y * self.cell_size, self.cell_size,
                                    self.cell_size)
+                pygame.draw.rect(screen, color, rect)
                 pygame.draw.rect(screen, WHITE, rect, 1)
 
-    def get_cell(self, nouse_pos):
-        x, y = nouse_pos
+    def get_color(self, value):
+        if value == 0:
+            return BLACK
+        elif value == 1:
+            return RED
+        elif value == 2:
+            return BLUE
+
+    def get_cell(self, mouse_pos):
+        x, y = mouse_pos
         x -= self.left
         y -= self.top
 
@@ -42,9 +56,13 @@ class Board:
     def handle_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell is not None:
-            print(cell)
-        else:
-            print(None)
+            self.toggle_colors(cell)
+
+    def toggle_colors(self, cell):
+        x, y = cell
+        current_color = self.board[y][x]
+        next_color = (current_color + 1) % 3
+        self.board[y][x] = next_color
 
     def run(self):
         clock = pygame.time.Clock()
